d93be23c15c39b85f288340fb048d45feb3b3094
vilichtSarina <sarina.lusti@noseryoung.ch>
2023-03-10 10:39:26 +0100

    created custom query

--- src/main/java/com/example/demo/domain/imagepost/ImagePostRepository.java
+++ src/main/java/com/example/demo/domain/imagepost/ImagePostRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.domain.imagepost;
 
 import com.example.demo.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,9 @@ import java.util.UUID;
 public interface ImagePostRepository extends JpaRepository<ImagePost, UUID> {
     List<ImagePost> findImagePostByAuthor(User author);
 
+    @Query(
+            value = "SELECT * FROM image_post WHERE image_post.image_url = ?1",
+            nativeQuery = true
+    )
     List<ImagePost> findImagePostByImageUrl(String imageUrl);
 }
